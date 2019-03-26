1eec893e78a87d3b30bebac71c0c723ec90aefb6
AnnaLoiko <amromany@gmail.com>
2019-03-26 23:39:47 +0300

    style: format indents

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-	let arrStr = str.split('');
-	let result;
-	
-	bracketsConfig.forEach(element => {
-		for ( let i = arrStr.length ; i >= 0 ; i--) {
+  let arrStr = str.split('');
+  let result;
+
+  bracketsConfig.forEach(element => {
+    for ( let i = arrStr.length ; i >= 0 ; i--) {
 
       if ( arrStr.lastIndexOf(element[0]) < 0) return;
-             
+              
       var indexOpen = (element[0]===element[1]) ? arrStr.indexOf(element[0]) : arrStr.lastIndexOf(element[0]) ;
       var indexClose = arrStr.indexOf(element[1], indexOpen+1);
 
@@ -19,8 +19,8 @@ module.exports = function check(str, bracketsConfig) {
       delete arrStr[indexOpen];
       result = true;
 
-		};
-	});
+    };
+  });
 
   var filterArrStr = arrStr.filter( e => typeof e == "string" );
   return result = (filterArrStr.length > 0) ? false : true ;
